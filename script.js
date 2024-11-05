2df690c364e1626843632d547bb484e3b6bc0362
MrGMTengineer <106160649+MrGMTengineer@users.noreply.github.com>
2024-11-05 19:39:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -14,16 +14,29 @@ var map = new ol.Map({
 
 // List of cities you visited with their coordinates
 var cities = [
-    { name: "Ankara", coordinates: [32.8597, 39.9334] },   // Longitude, Latitude for Ankara
-    { name: "Istanbul", coordinates: [28.9784, 41.0082] }, // Longitude, Latitude for Istanbul
-    { name: "Aksaray", coordinates: [34.0254, 38.3687] }   // Longitude, Latitude for Aksaray
+    { name: "Ankara", coordinates: [32.8597, 39.9334] },   // Ankara
+    { name: "İstanbul", coordinates: [28.9784, 41.0082] }, // İstanbul
+    { name: "Afyon", coordinates: [30.7067, 38.7569] },    // Afyon
+    { name: "Aksaray", coordinates: [34.0254, 38.3687] },  // Aksaray
+    { name: "Kırıkkale", coordinates: [33.5292, 39.8468] }, // Kırıkkale
+    { name: "Niğde", coordinates: [34.6794, 37.9668] },     // Niğde
+    { name: "Mersin", coordinates: [34.6398, 36.8121] },    // Mersin
+    { name: "Diyarbakır", coordinates: [40.2181, 37.9144] }, // Diyarbakır
+    { name: "Gaziantep", coordinates: [37.0662, 37.0662] },  // Gaziantep
+    { name: "Şırnak", coordinates: [42.4918, 37.4187] },    // Şırnak
+    { name: "Hakkari", coordinates: [43.7370, 37.5739] },   // Hakkari
+    { name: "Van", coordinates: [43.0560, 38.5012] },       // Van
+    { name: "Edirne", coordinates: [26.5530, 41.6761] },    // Edirne
+    { name: "Manisa", coordinates: [27.4265, 38.6125] },    // Manisa
+    { name: "Denizli", coordinates: [29.0875, 37.7833] },   // Denizli
+    { name: "Fildişi Sahili", coordinates: [-5.5471, 7.5396] } // Fildişi Sahili
 ];
 
 // Define star icon
 var iconStyle = new ol.style.Style({
     image: new ol.style.Icon({
-        src: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Red_star.svg', // Star icon URL
-        scale: 0.01, // Adjust scale for the icon size
+        src: 'https://www.svgrepo.com/show/527218/map-point.svg', // Star icon URL
+        scale: 0.05, // Adjust scale for the icon size
         anchor: [0.5, 1] // Anchor the icon so the bottom center is at the location
     })
 });
